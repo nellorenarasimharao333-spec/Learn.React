ad24acbef76cce9700e98db88de4e02ad3c29b0d
narasimha-Demo <nellorenarasimharao333@gmail.com>
2026-06-03 17:56:41 +0530

    Add 05-CSS sample folder

---
+++ 05-CSS/app.js
@@ -0,0 +1,5 @@
+const root = document.getElementById('root');
+const card = document.createElement('div');
+card.className = 'card';
+card.innerHTML = '<h2>CSS Card</h2><p>A small sample demonstrating CSS styling.</p>';
+root.appendChild(card);
